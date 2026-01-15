0ae7a5444fea74f723b57c73f0be915db77cdf3d
Christopher G <135375609+AI-Ocko@users.noreply.github.com>
2026-01-15 00:04:01 -0800

    Cleaned up the code I have now

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,26 +1,49 @@
+let humanScore = 0;
+let computerScore = 0;
+
+const humanSelection = getHumanChoice;
+const computerSelection = getComputerChoice;
+
 function getComputerChoice() {
   // generate random number, from 0 <= n < 1, store it in n
   let n = Math.random();
   // determine which option to choose based off generated number
   if (1 > n && n > 0.666) {
-    console.log("Rock")
+    return "rock"
   }
   else if (0.666 >= n && n > 0.333) {
-    console.log("Paper")
-  }
-  else if (0.333 >= n && n >=0) {
-    console.log("Scissors")
+    return "paper"
   }
   else {
-    console.log("Error")
+  return "scissors"
   }
 }
 
 function getHumanChoice () {
-  // set variable to user input
-  let humanChoice = prompt("Rock...Paper...Scissors...Shoot!");
-  console.log(humanChoice)
+  // set variable to user input, then make it lowercase
+  return prompt("Rock...Paper...Scissors...Shoot!").toLowerCase();
 }
 
-getComputerChoice();
-getHumanChoice();
+// function playRound(humanSelection, computerSelection) {
+//   if (humanSelection === computerSelection) {
+//     console.log("It's a tie!")
+//   }
+//   else {
+//     switch (humanSelection, computerSelection) {
+//       case humanSelection === "rock":
+//
+//         break;
+//
+//       default:
+//         break;
+//     }
+//
+//   }
+// }
+
+getHumanChoice()
+getComputerChoice()
+
+console.log(computerSelection)
+console.log(humanSelection)
+// playRound(humanSelection, computerSelection)
