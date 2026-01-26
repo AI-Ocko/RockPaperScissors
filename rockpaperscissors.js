b47f0932da32fd00f4e72b90dd018fd3cbe4a57c
Christopher G <135375609+AI-Ocko@users.noreply.github.com>
2026-01-26 12:52:20 -0800

    Changed webpage UI
    Now has clickable buttons, outcome message, and score tracker

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,3 +1,8 @@
+// These are selections for html elements so I can manipulate them later in this file
+const buttons = document.querySelectorAll("button");
+const resultText = document.querySelector("#result");
+const scoreText = document.querySelector("#score");
+
 let humanScore = 0;
 let computerScore = 0;
 
@@ -6,55 +11,42 @@ function getComputerChoice() {
   let n = Math.random();
   // determine which option to choose based off generated number
   if (1 > n && n > 0.666) {
-    return "rock"
+    return "rock";
   }
   else if (0.666 >= n && n > 0.333) {
-    return "paper"
+    return "paper";
   }
   else {
-  return "scissors"
+    return "scissors";
   }
-}
-
-function getHumanChoice() {
-  // set variable to user input, then make it lowercase
-  return prompt("Rock...Paper...Scissors...Shoot!").toLowerCase();
 }
 
 function playRound(humanChoice, computerChoice) {
   // check for a tie
   if (humanChoice === computerChoice) {
-    return "It's a tie!"
+    return "It's a tie!";
   }
   // check win possibilities
   else if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
-    humanScore++
-    return "You Win!" + " " + humanChoice + " beats" + " " + computerChoice
+    humanScore++;
+    return "You Win! " + humanChoice + " beats " + computerChoice;
   }
   // all other scenarios are losing
   else {
-    computerScore++
-    return "You Lose!" + " " + computerChoice + " beats" + " " + humanChoice
+    computerScore++;
+    return "You Lose! " + computerChoice + " beats " + humanChoice;
   }
 }
 
-function playGame (humanChoice, computerChoice) {
-  for (let i = 0; i < 5; i++) {
-    const humanSelection = getHumanChoice()
-    const computerSelection = getComputerChoice()
-    const result = playRound(humanSelection, computerSelection)
-    console.log(result)
-    console.log("Player: " + humanScore + "||" + "Computer: " + computerScore)
-  }
-  if (humanScore > computerScore) {
-    console.log("You Win! You managed to beat a semi random computer!!")
-  }
-  else {
-    console.log("You Lose... Don't feel bad, it's a computer anyways!")
-  }
-}
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const humanChoice = button.dataset.choice;
+    const computerChoice = getComputerChoice();
 
-// console.log(computerSelection)
-// console.log(humanSelection)
-// console.log(playRound(humanSelection, computerSelection))
-playGame()
+    const result = playRound(humanChoice, computerChoice);
+
+    resultText.textContent = result;
+    scoreText.textContent = "Player: " + humanScore + " | Computer: " + computerScore;
+  });
+});
+
